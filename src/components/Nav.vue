b59fedfbd93859cb8f0e4d39304206be5d8dc20e
yuxuewu <18329517675@163.com>
2022-07-14 21:04:27 +0800

    1

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
  * @Author: yuxuewu 18329517675@163.com
  * @Date: 2022-07-06 23:30:13
  * @LastEditors: yuxuewu 18329517675@163.com
- * @LastEditTime: 2022-07-13 22:58:44
+ * @LastEditTime: 2022-07-14 20:52:24
  * @FilePath: \admin-app\src\components\Nav.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -79,7 +79,7 @@ const handlePage = ({ key, name }) => {
   background: #b10100 !important;
   color: #fff;
   font-size: 24px;
-  width: 80%;
+  width: 60%;
   margin: 0 auto;
   display: flex;
   justify-content: start;
