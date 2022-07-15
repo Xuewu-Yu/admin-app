e8b8d74ae5677349abe55a9a162ad3dfff51e0bb
yuxuewu <18329517675@163.com>
2022-07-15 22:46:35 +0800

    1

--- src/components/boardCard.vue
+++ src/components/boardCard.vue
@@ -2,37 +2,21 @@
  * @Author: yuxuewu 18329517675@163.com
  * @Date: 2022-07-07 19:54:26
  * @LastEditors: yuxuewu 18329517675@163.com
- * @LastEditTime: 2022-07-12 18:56:49
+ * @LastEditTime: 2022-07-15 22:42:17
  * @FilePath: \admin-app\src\components\boardCard.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
   <div class="content">
     <div class="card-wrap">
-      <Card hoverable>
+      <!-- <Card hoverable>
         <template #cover>
-          <img src="../assets/person.png" alt="">
+          <img src="" alt="">
         </template>
         <Card.Meta title="title">
           <template #description>description</template>
         </Card.Meta>
-      </Card>
-      <Card hoverable>
-        <template #cover>
-          <img src="../assets/person.png" alt="">
-        </template>
-        <Card.Meta title="title">
-          <template #description>description</template>
-        </Card.Meta>
-      </Card>
-      <Card hoverable>
-        <template #cover>
-          <img src="../assets/person.png" alt="">
-        </template>
-        <Card.Meta title="title">
-          <template #description>description</template>
-        </Card.Meta>
-      </Card>
+      </Card> -->
     </div>
     <List >
       <List.Item>
