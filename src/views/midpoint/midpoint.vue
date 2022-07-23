4a452662fd941ac33267775504e569c34286ee09
yuxuewu <18329517675@163.com>
2022-07-23 14:53:10 +0800

    1

--- src/views/midpoint/midpoint.vue
+++ src/views/midpoint/midpoint.vue
@@ -2,7 +2,7 @@
  * @Author: yuxuewu 18329517675@163.com
  * @Date: 2022-07-20 21:24:47
  * @LastEditors: yuxuewu 18329517675@163.com
- * @LastEditTime: 2022-07-23 14:30:11
+ * @LastEditTime: 2022-07-23 14:52:12
  * @FilePath: \admin-app\src\views\midpoint\midpoint.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -16,10 +16,12 @@
       <Nav :menus="menus" v-model:tab="subModel" @handlePage="handlePage"></Nav>
     </div>
     <div class="content-wrap" :style="{ backgroundImage: `url(${banner.banner}` }">
-      <div class="wrapper">
+      <div class="wrapper" v-if="!subModel.includes('5')">
         <div v-if="html" v-html="html"></div>
-        <Empty v-else-if="!html && !subModel.includes('5')" :description="'暂无数据'" />
-        <VideoGroup v-if="subModel.includes('5')" />
+        <Empty v-else-if="!html" :description="'暂无数据'" />
+      </div>
+      <div v-if="subModel.includes('5')" class="video-wrapper">
+        <VideoGroup />
       </div>
     </div>
   </div>
@@ -73,4 +75,7 @@ handlePage('1');
 </script>
 <style lang="scss" scoped>
 @import '../characteristicindustry/components/varible.scss';
+.video-wrapper {
+  padding: 40px 0;
+}
 </style>
