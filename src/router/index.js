7a696114c78aa379d943e310e7cdc72da1e4990c
yuxuewu <18329517675@163.com>
2022-07-25 10:01:14 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: yuxuewu 18329517675@163.com
  * @Date: 2022-07-06 09:24:38
  * @LastEditors: yuxuewu 18329517675@163.com
- * @LastEditTime: 2022-07-24 19:06:34
+ * @LastEditTime: 2022-07-25 09:56:36
  * @FilePath: \admin-app\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,11 +21,11 @@ export const routes = [
     component: oldHome,
     name: 'oldHome',
   },
-  {
-    path: '/videoGroup',
-    component: () => import('@/views/videoGroup/videoGroup.vue'),
-    name: 'videoGroup',
-  },
+  // {
+  //   path: '/videoGroup',
+  //   component: () => import('@/views/videoGroup/videoGroup.vue'),
+  //   name: 'videoGroup',
+  // },
   {
     path:'/organization',
     component: () => import('@/views/organization/organization.vue'),
--- src/views/videoGroup/videoGroup.vue
+++ src/views/videoGroup/videoGroup.vue
@@ -2,7 +2,7 @@
  * @Author: yuxuewu 18329517675@163.com
  * @Date: 2022-07-23 11:18:23
  * @LastEditors: yuxuewu 18329517675@163.com
- * @LastEditTime: 2022-07-25 09:51:57
+ * @LastEditTime: 2022-07-25 10:01:05
  * @FilePath: \admin-app\src\views\videoGroup\videoGroup.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -64,7 +64,7 @@ const listData = computed(() => {
       position: absolute;
       color:#fff;
       font-size: 14px;
-      width: 100%;
+      width: 90%;
       z-index:999;
       margin: 0;
       // width: 300px;
