058f199df76ea03f0e9b5f8faa544c1ee8ebbf94
yuxuewu <18329517675@163.com>
2023-02-22 00:02:46 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@
  * @FilePath: \admin-app\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import Index from '@/views/index/index.vue';
+import Index from '@/views/index2/index2.vue';
 // import HelloWorld from '@/components/HelloWorld.vue';
 import oldHome from '@/views/oldHome/oldHome.vue';
 export const routes = [
---
+++ src/views/index2/index2.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="main-wrapper">
+    <div class="desc">
+      
+    </div>
+    <div class="top-right">
+      <!-- <image style="width:372px;height:175px" src="@/assets/home/rural_03.png" /> -->
+    </div>
+  </div>
+</template>
+<script setup>
+import { Image } from 'ant-design-vue';
+
+</script>
+<style lang="scss" scoped>
+.main-wrapper {
+  width: 100vw;
+  height:100vh;
+  background: url('@/assets/home/rural.png');
+  background-size: 100% 100%;
+  .desc {
+    position: absolute;
+    width: 118px;
+    height: 55px;
+    // margin: auto;
+    background: url('@/assets/home/rural_33.png');
+    background-size: 100% 100%;
+    bottom: 253px;
+    left: calc(50% - 0px);
+  }
+  .top-right {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 186px;
+    height: 67.5px;
+    background: url('@/assets/home/rural_03.png');
+    background-size: 100% 100%;
+  }
+}
+</style>
